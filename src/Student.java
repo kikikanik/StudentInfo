53f5daa176e9cf53f3a7fc9d801774a4730ea3bb
kikikanik <55113869+kikikanik@users.noreply.github.com>
2020-03-30 18:26:42 -0400

    Please Work- First Commit

---
+++ src/Student.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*; 
+//import java.util.temporal.ChronoUnit; 
+
+public class Student {
+	String firstName; 
+	String lastName; 
+	String middleInitial; 
+	int dob_day;  // dob - data of birth 
+	int dob_month; 
+	int dob_year;
+	
+	public String getFullName () {
+   
+		// This method should return the full name    
+		//    concatenating first name, middle initial and last name   ...
+		String fullName = firstName + middleInitial + lastName;
+		return fullName;
+		} 
+	public int age( ) {
+		int age = 0; 
+		LocalDate pdate = LocalDate.of(year, month, day);
+		LocalDate now = LocalDate.now();
+		
+		Period diff = Period.between(pdate, now);
+		age = diff.getYears();
+		return age;
+	}
+	public long elaspedDays (int y, int m, int d) {
+		long days = 0;
+		
+		LocalDate pdate = LocalDate.of(year,  month,  day);
+		LocalDate now = LocalDate.of(y,  m,  d);
+		days = java.time.temporal.ChronoUnit.DAYS.between(pdate, now);
+		return days;
+	}
+	}
+
---
+++ src/StudentTester.java
@@ -0,0 +1,13 @@
+
+public class StudentTester {
+
+	public static void main(String[] args) {
+		//have two separate students w diff names and dob
+		//invoke getFullName and age methods 
+		//compare the two to see which is older (output the older dude)
+		//invoke elapsedDays method for each student, passing arbitrary date. print output as Elaspsed Days: <days>
+
+
+	}
+
+}
